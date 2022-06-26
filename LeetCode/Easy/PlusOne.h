eac16dfda1b37bc84bb3912e3e223c871b64c905
Kharlamenko <kharlamenkodev@gmail.ru>
2022-06-26 13:38:41 +0300

    Added implementation of 'PlusOne' task

---
+++ LeetCode/Easy/PlusOne.h
@@ -0,0 +1,33 @@
+#include <vector>
+#include <string>
+
+using std::vector;
+
+class PlusOne
+{
+public:
+
+	vector <int> implementation(vector <int>& digits) {
+
+		auto it = digits.rbegin();
+		(*it)++;
+
+		while (it != digits.rend()){
+			if (*it == 10) {
+				*it = 0;
+				it++;
+				if (it == digits.rend()){
+					digits.insert(digits.cbegin(), 1);
+					break;
+
+				}
+				(*it)++;
+			}
+			else {
+				break;
+			}
+		}
+
+		return digits;
+	}
+};
