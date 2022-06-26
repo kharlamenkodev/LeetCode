8a092a95735dd18f5d7d001b384a20ee9a018824
Kharlamenko <kharlamenkodev@gmail.ru>
2022-06-26 12:50:45 +0300

    Added implementation of 'LengthOfLastWord' rask

---
+++ LeetCode/Easy/LengthOfLastWord.h
@@ -0,0 +1,36 @@
+#include <string>
+
+using std::string;
+
+class LengthOfLastWord
+{
+public:
+
+    int implementation(const string& s) const {
+
+        auto it = s.crbegin() ;
+        bool finded = false;
+        uint32_t count = 0;
+
+        while (it != s.crend()){
+            if (finded){
+                if (*it == ' '){
+                    break;
+                }
+                else{
+                    count++;
+                }
+            }
+            else{
+                if (*it != ' '){
+                    count++;
+                    finded = true;
+                }
+            }
+
+            it++;
+        }
+
+        return count;
+    }
+};
